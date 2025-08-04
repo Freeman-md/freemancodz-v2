0d9948593c51c01b7ddbe15e44d1bc24eeb1ae34
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-08-04 09:25:32 +0100

    refactor: reorganize imports and enhance button layout in SignInWithGoogle component

--- src/app/(auth)/auth/signin/page.tsx
+++ src/app/(auth)/auth/signin/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { IconBrandGoogle } from "@tabler/icons-react";
+import { IconArrowLeft, IconBrandGoogle } from "@tabler/icons-react";
 import { createClient } from "@/utils/supabase/client";
+import Link from "next/link";
 
 export default function SignInWithGoogle() {
-  const supabase = createClient()
+  const supabase = createClient();
 
   const handleSignIn = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
@@ -14,14 +15,28 @@ export default function SignInWithGoogle() {
         redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
-    
+
     if (error) console.error("Google Sign-in Error:", error.message);
   };
 
   return (
-    <Button onClick={handleSignIn} variant="outline" size="lg" className="cursor-pointer">
-      <IconBrandGoogle className="mr-2" />
-      Sign In With Google
-    </Button>
+    <div className="space-y-4 flex flex-col">
+      <Button
+        onClick={handleSignIn}
+        variant="outline"
+        size="lg"
+        className="cursor-pointer"
+      >
+        <IconBrandGoogle className="mr-2" />
+        Sign In With Google
+      </Button>
+
+      <Button asChild variant="ghost" className="cursor-pointer">
+        <Link href="/">
+          <IconArrowLeft />
+          Go Back Home
+        </Link>
+      </Button>
+    </div>
   );
 }
